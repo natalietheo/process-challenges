4231775950939cc5b9e2940ff515210af389d404
Natalie Theodoropoulou <natalie.theo@gmail.com>
2023-07-17 10:34:35 +0100

    all four tests passing

--- leap-years/leap-years.js
+++ leap-years/leap-years.js
@@ -1,4 +1,21 @@
 const isLeapYear = (year) => { 
-    if (year % 4 === 0)
-    return true
+    // if year is not divisible by 4, it is not a leap year
+    if (year % 4 !== 0)
+    return false;
+
+    // if year is divisible by 400, it is a leap year
+    if (year % 400 === 0)
+    return true;
+
+    // if year is divisible by 100 but not by 400, it is not a leap year
+    if (year % 100 === 0 && year % 400 !== 0)
+    return false;
+
+    // if year is divisible by 4 but not by 100, it is a leap year
+    if(year % 4 === 0 && year % 100 !== 0)
+    return true;
+
+   else return false;
 }
+
+module.exports = isLeapYear;
--- leap-years/leap-years.test.js
+++ leap-years/leap-years.test.js
@@ -4,4 +4,22 @@ describe('isLeapYear', () => {
     it('returns false if number is not divisible by 4', () => {
         expect(isLeapYear(2009)).toEqual(false)
     });
+
+    it('returns true if divisible by 400', () => {
+    expect(isLeapYear(2000)).toEqual(true)
 });
+
+    it('returns false if divisible by 100 and not divisble by 400', () => {
+    expect(isLeapYear(1700)).toEqual(false)
+    expect(isLeapYear(1800)).toEqual(false)
+    expect(isLeapYear(1900)).toEqual(false)
+    }
+    )
+
+    it('returns true if years divisible by 4 but not by 100', () => {
+        expect(isLeapYear(2004)).toEqual(true)
+        expect(isLeapYear(2008)).toEqual(true)
+        expect(isLeapYear(2012)).toEqual(true)
+    })
+});
+
